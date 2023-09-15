cf6e3be33949b026ab0213f1351bddc9b43d8b10
Marius Solbakken Mellum <marius.solbakken@amesto.no>
2023-09-15 09:45:28 +0200

    Fix paths

--- examples/exampleB/main.tf
+++ examples/exampleB/main.tf
@@ -25,6 +25,13 @@ module "vmss" {
   source                         = "c:/git/terraform-azurerm-selfhostedrunnervmss" # TODO
   operating_system               = "ubuntu"       # windows or ubuntu
   runner_platform                = "azure_devops" # azure_devops or github
+  resource_group_name            = azurerm_resource_group.rg.name
+  use_existing_resource_group    = true
+  location                       = azurerm_resource_group.rg.location
+  virtual_machine_scale_set_name = "runners"
+  sku                            = "Standard_D2s_v3"
+  ssh_public_keys                = ["ssh-rsa AAAAB3NzaC1yc2EAAAADA....QFv2PJ0= marius@42device"]
+  subnet_id                      = azurerm_subnet.vmss.id
 }
 
 output "password" {
