ce0acb27c4448c7e5598c8a8b7a0ea27ec83be2c
Ketil Gjerde <ketil.gjerde@amesto.no>
2023-10-25 14:10:02 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "self_hosted_runners" {
       name      = "internal"
       primary   = true
       subnet_id = var.subnet_id != null ? var.subnet_id : azurerm_subnet.vmss[0].id
-      load_balancer_backend_address_pool_ids = [var.load_balancer_backend_address_pool_id]
+      load_balancer_backend_address_pool_ids = var.load_balancer_backend_address_pool_id != "" ? [var.load_balancer_backend_address_pool_id] : null
     }
   }
 
@@ -136,7 +136,7 @@ resource "azurerm_windows_virtual_machine_scale_set" "self_hosted_runners" {
       name      = "internal"
       primary   = true
       subnet_id = var.subnet_id != null ? var.subnet_id : azurerm_subnet.vmss[0].id
-      load_balancer_backend_address_pool_ids = [var.load_balancer_backend_address_pool_id]
+      load_balancer_backend_address_pool_ids = var.load_balancer_backend_address_pool_id != "" ? [var.load_balancer_backend_address_pool_id] : null
     }
   }
 
