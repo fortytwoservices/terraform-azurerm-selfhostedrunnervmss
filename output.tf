a0bf442249b229e95eee93362ce3e575678a0feb
Ketil Gjerde <ketil.gjerde@amesto.no>
2023-12-06 12:13:18 +0100

    feat: output vmss id
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "password" {
   value     = local.password
   sensitive = true
 }
+
+output "virtual_machine_scale_set_id" {
+  value = var.operating_system == "ubuntu" ? azurerm_linux_virtual_machine_scale_set.self_hosted_runners[0].id : azurerm_windows_virtual_machine_scale_set.self_hosted_runners[0].id
+}
