c17a350b2501b23a0dc15ded3efe029723698924
Ketil Gjerde <ketil.gjerde@amesto.no>
2024-01-06 23:13:34 +0100

    fix. correct some default parameters
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- scripts/invoke-ghrunner.ps1
+++ scripts/invoke-ghrunner.ps1
@@ -6,8 +6,8 @@ param (
     [string]$user,
     [string]$userpassword,
     [string]$labels,
-    [switch]$replace,
-    [switch]$disableupdate,
+    [switch]$noreplace,
+    [switch]$enableupdate,
     [switch]$ephemeral,
     [string]$githubpat = $env:RUNNER_CFG_PAT
 )
@@ -27,9 +27,10 @@ $runnerurl = "https://github.com/$runnerscope"
 
 Expand-Archive -Path "C:\ProgramData\runner\actions-runner-win-x64-*.zip" -DestinationPath "C:\ProgramData\runner"
 
+# Build the argument list and configure the Github Actions Runner
 $argList = @("--unattended", "--url", $runnerurl, "--pat", $githubpat, "--WindowsLogonAccount", $user, "--WindowsLogonPassword", $userpassword, "--runasservice")
 
-if ($null -eq $replace || $replace) {
+if ($null -eq $noreplace) {
     $argList += "--replace"
 }
 if ($null -ne $labels) {
@@ -40,7 +41,7 @@ if ($null -ne $runner_group) {
     $argList += "--runnergroup"
     $argList += $runner_group
 }
-if ($null -eq $disableupdate || $disableupdate) {
+if ($null -eq $enableupdate) {
     $argList += "--disableupdate"
 }
 if ($null -ne $ephemeral) {
@@ -49,10 +50,12 @@ if ($null -ne $ephemeral) {
 
 Start-Process -FilePath .\config.cmd -ArgumentList $argList -NoNewWindow -Wait
 
+# Added to follow recommendations in https://github.com/actions/runner-images/blob/main/docs/create-image-and-azure-resources.md#post-generation-scripts
 $securePassword = ConvertTo-SecureString $userpassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $user, $securePassword
 Start-Process Notepad.exe -Credential $credential
 
 Get-ChildItem C:\post-generation -Filter *.ps1 | ForEach-Object { Start-Process $_.FullName -Credential $credential }
 
+# Starting the Github Actions Runner service
 Start-Service actions.runner.*
