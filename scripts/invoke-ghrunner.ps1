2eca58a2fc5f65a8eadeb83074403725d475ae05
Ketil Gjerde <ketil.gjerde@amesto.no>
2024-01-06 22:59:03 +0100

    fix: update command
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- scripts/invoke-ghrunner.ps1
+++ scripts/invoke-ghrunner.ps1
@@ -29,8 +29,35 @@ $runnerurl = "https://github.com/$runnerscope"
 # Unzip archive *.zip
 Expand-Archive -Path "C:\ProgramData\runner\actions-runner-win-x64-*.zip" -DestinationPath "C:\ProgramData\runner"
 
+# Initialize the argument list with mandatory arguments
+$argList = @("--unattended", "--url", $runnerurl, "--pat", $githubpat, "--WindowsLogonAccount", $user, "--WindowsLogonPassword", $userpassword, "--runasservice")
+
+# Add optional arguments based on conditions
+if ($null -eq $replace || $replace) {
+    $argList += "--replace"
+}
+if ($null -ne $labels) {
+    $argList += "--labels"
+    $argList += $labels
+}
+if ($null -ne $runner_group) {
+    $argList += "--runnergroup"
+    $argList += $runner_group
+}
+if ($null -eq $disableupdate || $disableupdate) {
+    $argList += "--disableupdate"
+}
+if ($null -ne $ephemeral) {
+    $argList += "--ephemeral"
+}
+
+# Run the config.cmd script with the argument list
+Start-Process -FilePath .\config.cmd -ArgumentList $argList -NoNewWindow -Wait
+
+#Get-ChildItem C:\post-generation -Filter *.ps1 | ForEach-Object { & $_.FullName }
+
 # Run command config.cmd
-& .\config.cmd --unattended --url $runnerurl --pat $githubpat --WindowsLogonAccount $user --WindowsLogonPassword $userpassword --replace --disableupdate --runasservice
+# & .\config.cmd --unattended --url $runnerurl --pat $githubpat --WindowsLogonAccount $user --WindowsLogonPassword $userpassword --replace --disableupdate --runasservice
 
 # Start service
 Start-Service actions.runner.*
