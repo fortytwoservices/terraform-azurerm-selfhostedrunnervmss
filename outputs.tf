d3892c843b9cff89538bef75a167a60a3adeac14
Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>
2024-08-07 13:28:09 +0200

    feat: added output of the identity principal id
    Signed-off-by: Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "password" {
 output "virtual_machine_scale_set_id" {
   value = var.operating_system == "ubuntu" ? azurerm_linux_virtual_machine_scale_set.self_hosted_runners[0].id : azurerm_windows_virtual_machine_scale_set.self_hosted_runners[0].id
 }
+
+output "virtual_machine_scale_set_identity_principal_id" {
+  value = var.operating_system == "ubuntu" ? azurerm_linux_virtual_machine_scale_set.self_hosted_runners[0].identity.principal_id : azurerm_windows_virtual_machine_scale_set.self_hosted_runners[0].identity.principal_id
+}
