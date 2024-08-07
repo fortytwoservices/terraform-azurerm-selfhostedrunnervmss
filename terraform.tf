60905a4d4b19dbeacf22a5e75b37f9120bd0c412
Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>
2024-08-07 15:00:22 +0200

    feat: added terraform block with required providers and versions to satisfy the lint gods
    Signed-off-by: Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm {
+      source  = "hashicorp/azurerm"
+      version = "3.114.0"
+    }
+
+    random {
+      source  = "hashicorp/random"
+      version = "3.6.2"
+    }
+  }
+}
