330f7ce33971f78ebc1369a159164e4ceecd070c
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2024-09-16 13:11:29 +0200

    feat: Add subnet_id as output
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@ output "virtual_machine_scale_set_id" {
 output "virtual_machine_scale_set_identity_principal_id" {
   value = var.identity != null ? (var.operating_system == "ubuntu" ? azurerm_linux_virtual_machine_scale_set.self_hosted_runners[0].identity[0].principal_id : azurerm_windows_virtual_machine_scale_set.self_hosted_runners[0].identity[0].principal_id) : "Identity is not enabled"
 }
+
+output "subnet_id" {
+  value = var.subnet_id != null ? var.subnet_id : azurerm_subnet.vmss[0].id
+}
